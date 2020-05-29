730acf4b52d0e516cb2e729058e732b798ea85b2
ZebraStudioSAS <61851310+ZebraStudioSAS@users.noreply.github.com>
2020-05-29 08:56:03 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/*function cancelarPedido() {
+    if (confirm('Esta seguro de cancelar el pedido?')) {
+        // Save it!
+        console.log('Thing was saved to the database.');
+    } else {
+        // Do nothing!
+        console.log('Thing was not saved to the database.');
+    }
+}*/
